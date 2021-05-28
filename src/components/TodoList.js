4bba02ddcfb3aa7855febc990a826e56c91d331d
johann gomez <johanngomez02@gmail.com>
2021-05-27 21:22:31 -0500

    creando componente TodoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TodoList = () =>{
+    return (
+        <>
+           <div className="header text-center">
+              <h3>Agenda Personal</h3>
+              <button className="btn btn-primary mt-2">Crear tarea</button>
+           </div>
+           <div className="task-container">
+
+           </div>
+        </>
+    );
+};
+
+export default TodoList;
