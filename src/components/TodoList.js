7ef56cabd3f36583389a2686726a87cd617f605f
johann gomez <johanngomez02@gmail.com>
2021-05-27 23:39:51 -0500

    creando createTask (ventana emergente)

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CreateTask from '../modals/createTask';
 
-const TodoList = () =>{
+const TodoList = () => {
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => {
+        setModal(!modal);
+    }
+    
     return (
         <>
            <div className="header text-center">
               <h3>Agenda Personal</h3>
-              <button className="btn btn-primary mt-2">Crear tarea</button>
+              <button className="btn btn-primary mt-2" onClick = {() => setModal(true)}>Crear tarea</button>
            </div>
            <div className="task-container">
-
+                
            </div>
+           <CreateTask toggle={toggle} modal={modal} />
         </>
     );
 };
