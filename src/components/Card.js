3d2515ae0a4cff7dda098496e651c707e0a8c436
johann gomez <johanngomez02@gmail.com>
2021-05-28 20:47:22 -0500

    botones eliminar y editar tareas, funcinalidad eliminar tarea

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Card = ({taskObj, index}) =>{
+const Card = ({taskObj, index, deleteTask, updateListArray}) => {
+    const [modal, setModal] = useState(false);
+
+    const colors = [
+        {
+            primaryColor : "#5D93E1",
+            secondaryColor : "#ECF3FC"
+        },
+        {
+            primaryColor : "#F9D288",
+            secondaryColor : "#FEFAF1"
+        },
+        {
+            primaryColor : "#5DC250",
+            secondaryColor : "#F2FAF1"
+        },
+        {
+            primaryColor : "#F48687",
+            secondaryColor : "#FDF1F1"
+        },
+        {
+            primaryColor : "#B964F7",
+            secondaryColor : "#F3F0FD"
+        }
+    ]
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const updateTask = (obj) => {
+        updateListArray(obj, index)
+    }
+
+    const handleDelete = () => {
+        deleteTask(index)
+    }
+
     return (
         <div>
             <div class = "card-wrapper mr-5">
@@ -14,7 +51,7 @@ const Card = ({taskObj, index}) =>{
                     <i class="fas fa-trash-alt" style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
                 </div>
             </div>
-            <EditTask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj}/>
+            
             </div>
         </div>
     );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,14 +5,31 @@ import Card from './Card';
 const TodoList = () => {
     const [modal, setModal] = useState(false);
     const [taskList, setTaskList] = useState([])
-
+    
     useEffect(() => {
         let arr = localStorage.getItem("taskList")
+       
         if(arr){
             let obj = JSON.parse(arr)
             setTaskList(obj)
         }
     }, [])
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
 
     const toggle = () => {
         setModal(!modal);
@@ -25,15 +42,15 @@ const TodoList = () => {
         setTaskList(taskList)
         setModal(false)
     }
-    
+
     return (
         <>
             <div className = "header text-center">
-                <h3>Agenda de tareas</h3>
-                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Crear tarea</button>
+                <h3>Todo List</h3>
+                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
             </div>
             <div className = "task-container">
-            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} /> )}
+            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
             </div>
             <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
         </>
